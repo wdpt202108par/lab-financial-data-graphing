4f73de2cf1d1db2b137965a43762d56de4c549b4
Yooyoo56 <tak.yoojeong@gmail.com>
2021-11-27 17:09:44 +0100

    iteration 3 done

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,6 +1,7 @@
 
+//server client
 /* Iteration 1 : GET the data - Axois request URL */
-axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-10-27&end=2021-11-26`)
 .then(responseAPI => {
 console.log('Response from API is: ', responseAPI.data);
 })
@@ -8,20 +9,41 @@ console.log('Response from API is: ', responseAPI.data);
 console.log('Error! Axios get failed error');
 })
 
+//iteration 3  (select, input)
+const inputFrom = document.getElementById('date-from')
+const inputTo = document.getElementById('date-to')
+inputFrom.onchange = function (event){
+    console.log('HELLO this is the inputFrom')
+
+    // redessiner mon chart avec la date from et to modifier
+    let from = '2021-06-11';
+    let to = '2021-11-27';
+    getDataAndCreateChart(from, to)
+
+}
+
+inputTo.onchange = function (event){
+
+    // redessiner mon chart avec la date from et to modifier
+    console.log('HELLO this is the inputTo')
+}
 
 //iteration 2
-axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+function getDataAndCreateChart(from, to){
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${from}&end=${to}`)
 .then (responseChart =>{
     printTheChart(responseChart.data);
 })
 .catch (err =>{
     console.log('Error! chart printing error====>',{err});
 })
+}
+
 
 function printTheChart(bitcoinData){
 const dailyData = bitcoinData['bpi'];
 const coinDates = Object.keys(dailyData);
-  const coinPrices = Object.values(dailyData); // object.values
+const coinPrices = Object.values(dailyData); // object.values
 console.log('coinPrices ====>', coinPrices);
 
 const ctx = document.getElementById('my-chart').getContext('2d');
@@ -54,3 +76,8 @@ const chart = new Chart(ctx, {
     }
 });
 }
+let from = '2021-06-11';
+let to = '2021-11-27';
+
+
+getDataAndCreateChart(from, to)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 const router = require("express").Router();
 
-// iteration 1
-// axios.<method> will now provide autocomplete and parameter typings
-const axios = require('axios').default;
-
 
 /* GET home page */
 router.get("/", (req, res, next) => {
